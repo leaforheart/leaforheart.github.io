6e91d18ec470fa04facb93f069606f57e62b6997
xiaoyilin <leaforbook@gmail.com>
2016-09-01 23:44:43 +0800

    add architecture.html

--- blog/js/shiro.js
+++ blog/js/shiro.js
@@ -6,11 +6,13 @@ var data = [
             {name:'翻译',
                 children:[
                     {
-                        name:'<a href="/leaforheart.github.io/blog/shiro/translate/introduction.html">1 Shiro简介</a>',
+                        name:'<a href="/leaforheart.github.io/blog/shiro/translate/introduction.html">1 Apache Shiro简介</a>',
                     },{
-                        name:'<a href="/leaforheart.github.io/blog/shiro/translate/terminology.html">2 Shiro术语</a>',
+                        name:'<a href="/leaforheart.github.io/blog/shiro/translate/terminology.html">2 Apache Shiro术语</a>',
                     },{
-                        name:'<a href="/leaforheart.github.io/blog/shiro/translate/tutorial.html">3 Shiro简短教程</a>',
+                        name:'<a href="/leaforheart.github.io/blog/shiro/translate/tutorial.html">3 Apache Shiro教程</a>',
+                    },{
+                        name:'<a href="/leaforheart.github.io/blog/shiro/translate/architecture.html">4 Apache Shiro架构</a>',
                     }
                 ]
             },
